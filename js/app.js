e05b92d3764a4f1766de63079a162c61bc076b4b
agohorel <alex.oddlogic@gmail.com>
2019-03-07 16:50:10 -0500

    add boids to app.js, splitscreen

--- js/app.js
+++ js/app.js
@@ -60,10 +60,12 @@ function draw() {
 		particles[i].display();
 	}
 
+
+	runBoids();
 }
 
 function Particle() {
-	this.pos = createVector(random(width), random(height));
+	this.pos = createVector(random(width/2, width), random(height));
 	this.vel = createVector(0, 0);
 	this.acc = createVector(0, 0);
 	this.maxSpeed = random(1, 6);
@@ -94,9 +96,9 @@ function Particle() {
 	}
 
 	this.edges = function() {
-		if (this.pos.x > width) this.pos.x = 0;
+		if (this.pos.x > width) this.pos.x = width/2;
 		if (this.pos.y > height) this.pos.y = 0;
-		if (this.pos.x < 0) this.pos.x = width;
+		if (this.pos.x < width/2) this.pos.x = width/2;
 		if (this.pos.y < 0) this.pos.y = height;
 	}
 
@@ -113,7 +115,7 @@ function Particle() {
 		
 
 		line(this.pos.x, this.pos.y, this.pos.x, this.pos.y);
-		line(width - this.pos.x, height - this.pos.y, width - this.pos.x, height - this.pos.y);
+		line(width/2 - this.pos.x, height - this.pos.y, width/2 - this.pos.x, height - this.pos.y);
 	}
 }
 
@@ -125,6 +127,93 @@ function showNoiseField(x, y, v){
 	line(0, 0, scale, 0);
 	pop();
 }
+
+/// BOIDS ///
+/// BOIDS ///
+/// BOIDS ///
+/// BOIDS ///
+
+let flock = [];
+let num_boids = 15;
+
+function runBoids(){
+	colorMode(HSB);
+
+	if (flock.length < num_boids && frameCount % 2 === 0){
+		flock.push(new Boid());
+	}
+
+	for (let boid of flock){
+		boid.update();
+		boid.edges();
+		boid.lines(flock);
+	}
+}
+
+class Boid {
+	constructor(){
+		this.position = createVector(random(width/2), random(height));
+		this.radius = random(3, 6);
+		this.hue = random(0, 180);
+		this.saturation = random(0, 100);
+		this.brightness = random(0, 100);
+		this.velocity = p5.Vector.random2D();
+		this.velocity.setMag(random(.01, 5));
+		this.acceleration = createVector();
+		this.maxSpeed = 10;
+		this.perception = random(25, width/3);
+		this.thickness = random(this.radius*.15, this.radius*.5);
+		this.getsBrighter = true;
+		this.getsBigger = true;
+		this.maxRadius = random(6, 12);
+		this.minRadius = random(1, 3);
+	}
+
+	edges(){
+		if (this.position.x > width/2 + this.radius){
+			this.position.x = 0;
+		} else if (this.position.x < -this.radius){
+			this.position.x = width;
+		}
+
+		if (this.position.y > height + this.radius){
+			this.position.y = 0;
+		} else if (this.position.y < -this.radius){
+			this.position.y = height;
+		}
+	}
+
+	lines(boids){
+		stroke(osc(this.hue, 360), osc(this.saturation, 100), osc(this.brightness, 100), osc(this.hue, 100));
+		strokeWeight(this.thickness);
+		let perceptionRadius = this.perception;
+		for (let other of boids){
+			let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+			// check if surrounding boids are within "perceivable" range and that "other" is not "me"/"this"
+			if (d < perceptionRadius && d > perceptionRadius*.75 && other != this){
+				rotate(radians(osc(this.hue), 360));
+				line(this.position.x, this.position.y, other.position.x, other.position.y);
+			}
+		}
+	}
+
+	update(){
+		this.position.add(this.velocity);
+		this.velocity.add(this.acceleration);
+		this.velocity.limit(this.maxSpeed);
+		this.acceleration.set(0, 0); // reset acceleration after each update
+		this.radius = osc(this.hue, 2);
+		this.thickness = this.radius*.5;
+		this.hue += .0001;
+		this.saturation += random(.001, 1);
+		this.brightness += random(.001, 1);
+	}
+}
+
+function osc(angle, scalar){
+	return abs(sin(radians(angle)) * scalar);
+}
+
 
 function windowResized() {
 	w = windowWidth;
