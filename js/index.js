158d5efd813a80826d2e36983ab3b21daf0abbe5
agohorel <alex.oddlogic@gmail.com>
2019-03-26 15:18:10 -0400

    improved dropdown
    note to self: DRY up js

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const webDevCategory = document.getElementById("works-webdev");
 const graphicsCategory = document.getElementById("works-graphics");
 
 const dropdownWeb = document.getElementById("dropdown-web");
+const dropdownDesign = document.getElementById("dropdown-design");
 
 webDevCategory.addEventListener("mouseover", () => {
     dropdownWeb.classList.remove("dropdown__fadeOut");
@@ -22,3 +23,26 @@ dropdownWeb.addEventListener("mouseout", () => {
     dropdownWeb.classList.remove("dropdown__fadeIn");
     dropdownWeb.classList.add("dropdown__fadeOut");
 });
+
+// design category 
+
+graphicsCategory.addEventListener("mouseover", () => {
+    dropdownDesign.classList.remove("dropdown__fadeOut");
+    dropdownDesign.classList.add("dropdown__fadeIn");
+});
+
+graphicsCategory.addEventListener("mouseout", () => {
+    dropdownDesign.classList.remove("dropdown__fadeIn");
+    dropdownDesign.classList.add("dropdown__fadeOut");
+});
+
+dropdownDesign.addEventListener("mouseover", () => {
+    dropdownDesign.classList.remove("dropdown__fadeOut");
+    dropdownDesign.classList.add("dropdown__fadeIn");
+});
+
+dropdownDesign.addEventListener("mouseout", () => {
+    dropdownDesign.classList.remove("dropdown__fadeIn");
+    dropdownDesign.classList.add("dropdown__fadeOut");
+});
+
