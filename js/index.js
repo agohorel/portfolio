e9441fb5c4d172bab90c376dedafe5f4db9cc572
agohorel <alex.oddlogic@gmail.com>
2019-03-27 13:52:59 -0400

    add animation to dropdown

--- js/index.js
+++ js/index.js
@@ -10,22 +10,31 @@ const dropdowns = [dropdownWeb, dropdownDesign];
 for (let i = 0; i < categories.length; i++){
     categories[i].addEventListener("mouseover", () => {
         toggleVisibility(dropdowns[i]);
+        animateDropdowns(dropdowns[i]);
     });
 
     categories[i].addEventListener("mouseout", () => {
         toggleVisibility(dropdowns[i]);
+        animateDropdowns(dropdowns[i]);
     });
 
     dropdowns[i].addEventListener("mouseover", () => {
         toggleVisibility(dropdowns[i]);
+        animateDropdowns(dropdowns[i]);
     });
 
     dropdowns[i].addEventListener("mouseout", () => {
         toggleVisibility(dropdowns[i]);
+        animateDropdowns(dropdowns[i]);
     });
 }
 
 function toggleVisibility(element){
     element.classList.toggle("dropdown__fadeIn");
     element.classList.toggle("dropdown__fadeOut");
+}
+
+function animateDropdowns(element) {
+    element.classList.toggle("dropdown__slide-in");
+    element.classList.toggle("dropdown__slide-out");
 }
