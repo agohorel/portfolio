ad26e84704a969718e4fb207bcc20061aee2b6c8
agohorel <alex.oddlogic@gmail.com>
2019-04-08 12:20:25 -0400

    improve dropdown menu
    automatically close dropdown on clicking dropdown item to improve mobile experience

--- js/index.js
+++ js/index.js
@@ -9,37 +9,36 @@ const categories = [webDevCategory, graphicsCategory];
 const dropdowns = [dropdownWeb, dropdownDesign];
 
 for (let i = 0; i < categories.length; i++){
-    categories[i].addEventListener("mouseover", () => {
-        toggleVisibility(dropdowns[i]);
-        animateDropdowns(dropdowns[i]);
+    categories[i].addEventListener("click", function() {
+        let target = this.parentElement.children[1];
+        toggleVisibility(target);
+
+        // get other dropdown than the one we just clicked
+        let other = this.parentElement.nextElementSibling || this.parentElement.previousElementSibling;
+        let otherTarget = other.children[1];
+        forceHidden(otherTarget);
     });
 
-    categories[i].addEventListener("mouseout", () => {
-        toggleVisibility(dropdowns[i]);
-        animateDropdowns(dropdowns[i]);
-    });
 
-    dropdowns[i].addEventListener("mouseover", () => {
-        toggleVisibility(dropdowns[i]);
-        animateDropdowns(dropdowns[i]);
-    });
-
-    dropdowns[i].addEventListener("mouseout", () => {
-        toggleVisibility(dropdowns[i]);
-        animateDropdowns(dropdowns[i]);
+    dropdowns[i].addEventListener("click", function() {
+        let target = this.parentElement.children[1];
+        forceHidden(target);
     });
 }
 
 function toggleVisibility(element){
     element.classList.toggle("dropdown__fadeIn");
     element.classList.toggle("dropdown__fadeOut");
-}
-
-function animateDropdowns(element) {
     element.classList.toggle("dropdown__slide-in");
     element.classList.toggle("dropdown__slide-out");
 }
 
+function forceHidden(elem) {
+    elem.classList.remove("dropdown__fadeIn");
+    elem.classList.add("dropdown__fadeOut");
+    elem.classList.remove("dropdown__slide-in");
+    elem.classList.add("dropdown__slide-out");
+}
 
 const dropdownLinks = document.querySelectorAll(".dropdown__link");
 // portfolio containers
